ef5a092eec1d2a0940cdbce46ca52a2e0157ea5e
shumvlesu <sashumihin@gmail.com>
2021-02-06 16:44:55 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+  public static void main(String[] args) {
+    //https://github.com/xerial/sqlite-jdbc
+    //https://habr.com/ru/sandbox/88039/
+    //одно соединение с бд
+    try (Connection conn = DriverManager.getConnection("jdbc:sqlite:D:/GeekBrains/BD/testBD.s3db")) {
+      Statement statement = conn.createStatement();
+
+
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+  }
+}
